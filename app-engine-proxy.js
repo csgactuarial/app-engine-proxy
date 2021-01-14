e1c0081b5a105389a1b09322ef75d710414c5916
tcross <tcross@csgactuarial.com>
2021-01-14 11:22:38 -0600

    pass cert parts directly into the server

--- app-engine-proxy.js
+++ app-engine-proxy.js
@@ -1,55 +1,12 @@
 var https = require('https');
 var url = require('url');
 var httpProxy = require('http-proxy');
-var secretManager = require('./secret-manager');
-
-var httpsOptions = {
-  key: null,
-  cert: null,
-  ca: null
-}
-
-/**
- * looks for a config.json file and builds the HTTPSOptions
- * for client and server.
- * {
- *   "key": path to private key,
- *   "cert": path to certificate,
- *   "ca": path to certificate authority chain
- * }
- */
-function buildHTTPSOptionsFromFiles() {
-  var fs = require('fs');
-
-  var getHTTPSOptions = function () {
-    return JSON.parse(fs.readFileSync('config.json', 'utf8')).keyCert;
-  }
-
-  httpsOptions = {
-    key: fs.readFileSync(getHTTPSOptions().key),
-    cert: fs.readFileSync(getHTTPSOptions().cert),
-    ca: fs.readFileSync(getHTTPSOptions().ca)
-  }
-}
-
-/**
- * Builds the HTTPS Options for server and cleint SSL
- * Uses google cloud secret manager
- * 
- * @return {Promise[undefined, undefined, undefined]}
- */
-async function buildHTTPSOptionsFromSecretManager() {
-  let p1 = secretManager.getProxyClientPrivateKey().then(key => { httpsOptions.key = key });
-  let p2 = secretManager.getProxyClientCRT().then(cert => { httpsOptions.cert = cert });
-  let p3 = secretManager.getProxyClientCA().then(ca => { httpsOptions.ca = [ca] });
-
-  return Promise.all([p1, p2, p3]);
-}
+var httpsOptionsClient = require('./https-options')
 
 /**
  * Starts the proxy server, requires httpsOptions
  */
-function startServer() {
+function startServer(httpsOptions) {
   var respond = function (status, msg, res) {
     res.writeHead(status, { 'content-type': 'text/plain' });
     res.write(msg);
@@ -96,4 +53,6 @@ function startServer() {
   server.listen(443);
 }
 
-buildHTTPSOptionsFromSecretManager().then((values) => startServer());
+httpsOptionsClient.buildFromSecretManager().then(httpsOptions => {
+  startServer(httpsOptions);
+});
